2ec21c04e8938b07766feb949e0c9bf2b95072cf
rsork34 <ryan.sorkin34@gmail.com>
2021-12-16 11:43:26 -0500

    Added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./style/Header.css";
 
-function Header() {
+function Header(props) {
     return (
-        <div>
-            <h1>HEADER</h1>
+        <div id="mainHeader">
+            <div id="titleContainer">
+                <h1>Welcome to the e commerce app</h1>
+            </div>
+            <div id="shoppingCartContainer">
+                <p>Items in Cart: {props.numItems}</p>
+                <img id="shoppingCart" src={shoppingCart} alt="Shopping Cart Icon"  width="50px" height="50px" />
+            </div>
         </div>
     );
 }
