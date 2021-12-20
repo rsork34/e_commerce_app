81c2e01b837be31a3125ba3dbf6643e7f746991d
rsork34 <ryan.sorkin34@gmail.com>
2021-12-20 11:12:34 -0500

    Removed item count from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./style/Header.css";
 import shoppingCart from "../images/shopping-cart.png";
 
-function Header(props) {
+function Header() {
     return (
         <div id="mainHeader">
             <div id="titleContainer">
                 <h1>Welcome to the e commerce app</h1>
             </div>
             <div id="shoppingCartContainer">
-                <p>Items in Cart: {props.numItems}</p>
                 <img id="shoppingCart" src={shoppingCart} alt="Shopping Cart Icon"  width="50px" height="50px" />
             </div>
         </div>
