41ce21274ce71a551ca13eef14ec3a979da5617b
Ryan Sorkin <ryan.sorkin34@gmail.com>
2021-12-20 11:18:16 -0500

    modified default items list

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,43 +2,53 @@ import React, { useState } from "react";
 import SaleItem from "./SaleItem";
 import Header from "./Header";
 
-function Home(props) {
-    const defaultItems = [
+function Home() {
+    const items = [
         {
             name: "Item 1",
             description: "Item 1 Description",
-            price: 4.99
+            price: 4.99,
         },
         {
             name: "Item 2",
             description: "Item 2 Description",
-            price: 4.99
+            price: 4.99,
         },
         {
             name: "Item 3",
             description: "Item 3 Description",
-            price: 4.99
+            price: 4.99,
         },
         {
             name: "Item 4",
             description: "Item 4 Description",
-            price: 4.99
+            price: 4.99,
+        },
+        {
+            name: "Item 5",
+            description: "Item 5 Description",
+            price: 4.99,
+        },
+        {
+            name: "Item 6",
+            description: "Item 6 Description",
+            price: 4.99,
         },
     ];
-    const [items, setItems] = useState(defaultItems);
+    // const [items, setItems] = useState(defaultItems);
 
     return (
         <>
             <Header/>
 
             <div className="itemContainer">
-            {items.map((item) => (
+                {items.map((item) => (
                     <SaleItem
                         name={item.name}
                         desc={item.description}
                         price={item.price}
                     />
-            ))}
+                ))}
             </div>
         </>
     );
